604f3d63061cc26fcfbe2eb5817163897a5aa313
Shaharoz Ali Khan <shahrozdomore@gmail.com>
2021-05-30 03:38:42 +0500

    initial commit

---
+++ src/components/inline/InlineComp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Hi, I have 2 inline components</h1>
+    <MyButton />
+    <UserName name="Demi more" />
+  </div>
+</template>
+
+<script>
+//Inline component with template string
+const MyButton = {
+  template: `<h1>My Button</h1>`,
+};
+
+//Inline component with template string and props
+const UserName = {
+  props: ["name"],
+  template: "<p>Hi {{ name }}</p>",
+};
+
+export default {
+  components: {
+    MyButton,
+    UserName,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <InlineComp />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import InlineComp from "../components/inline/InlineComp.vue";
+
+export default {
+  name: "Home",
+  components: {
+    InlineComp,
+  },
+};
+</script>
